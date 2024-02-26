7e61f1d70a6403b6d9e5e8c4729bf2f6b3959957
Roy Francis <roymathewfrancis@gmail.com>
2024-02-26 22:58:25 +0100

    Added new layouts
    - Changed type to layout
    - Added new layouts
    - Changed default gap to 5px
    - Added border_radius
    - Removed box layout

--- R/pixgallery.R
+++ R/pixgallery.R
@@ -2,39 +2,60 @@
 #' @title Create an image gallery.
 #' @description Create an image gallery.
 #' @param path A character vector of full paths to images.
-#' @param caption A character vector of captions for the images (Optional).
+#' @param caption A character vector of captions for the images equal to length of path.
 #' @param dim Image gallery thumbnail dimension as a string in valid css units.
 #' @param gap Gap between thumbnails as a string in valid css units.
+#' @param border_radius Corner radius of thumbnails as a string in valid css units.
 #' @param shuffle A logical indicating whether images are randomly shuffled.
-#' @param type A character indicating gallery type. "box" or "grid" for now.
+#' @param layout A character indicating gallery layout type. "grid", "gridfixed", "masonry", "justified", "elastic" or "rhombus". See details.
 #' @param height Height of the widget as a string in valid css units.
 #' @param width Width of the widget as a string in valid css units.
 #' @param elementId A character denoting parent container ID.
 #' @importFrom htmlwidgets createWidget sizingPolicy
+#' @details 
+#' grid: Square grid responsive layout. Adjust square size using 'dim'. Width is flexible to fit parent.\n
+#' gridfixed: Same as grid but strictly fixed dimensions.\n
+#' masonry: Column responsive layout with fixed width. Adjust column width using 'dim'.\n
+#' justified: Similar to masonry but rows have fixed height. Adjust row height using 'dim'.\n
+#' elastic: Single row layout where images are magnified on hover.\n
+#' rhombus: Diamond shaped layout with three columns. Size and number of columns are fixed. Gap can be adjusted.
 #' @export
 #'
-pixgallery <- function(path, caption = NULL, dim = "200px", gap = "6px", shuffle = FALSE,
-                        type="grid", width = "100%", height = "100%", elementId = NULL) {
+pixgallery <- function(
+  path, 
+  caption = NULL, 
+  dim = "200px", 
+  gap = "5px", 
+  border_radius = "0px", 
+  shuffle = FALSE,
+  layout="grid", 
+  width = "100%", 
+  height = "100%", 
+  elementId = NULL
+  ) {
 
+  # check caption
   if(!is.null(caption)) {
-    if(length(caption) != length(path)) stop(paste("Length of 'caption' (", length(caption), ") is not not equal to the length of 'path' (", length(path), "). If 'caption' is used, it must be the same length as 'path'."))
+    if(length(caption) != length(path)) stop(paste0("Length of 'caption' (", length(caption), ") is not not equal to the length of 'path' (", length(path), "). If 'caption' is used, it must be the same length as 'path'."))
   } else {
     if(!is.null(names(path))) caption <- names(path)
   }
 
   names(path) <- NULL
 
-  types <- c("box","grid")
-  if(!type %in% types) stop(paste0("Argument type must be one of '",paste(types,collapse="', '"),"'."))
+  layouts <- c("grid", "gridfixed", "rhombus", "elastic", "masonry", "justified")
+  if(!layout %in% layouts) stop(paste0("Argument 'layout' must be one of '",paste(layouts,collapse="', '"),"'."))
 
   # forward options using x
   x = list(
     path = as.list(path),
     caption = as.list(caption),
+    link = as.list(link),
     dim = dim,
     gap = gap,
+    border_radius = border_radius,
     shuffle = shuffle,
-    type = type
+    layout = layout
   )
 
   # create widget
--- inst/htmlwidgets/pixgallery.js
+++ inst/htmlwidgets/pixgallery.js
@@ -36,36 +36,12 @@ function shuffle(obj1, obj2) {
 
 function pixgallery_base(el,x){
   if(x.shuffle) shuffle(x.path, x.caption);
-  if(x.type === "box") pixgallery_box(el,x);
-  if(x.type === "grid") pixgallery_grid(el,x);
-}
-
-/* flex box parent and children divs have images as img --------------------- */
-function pixgallery_box(el,x){
-
-  let urls = x.path;
-  let caption = x.caption;
-  let dim = x.dim;
-  let gap = x.gap;
-
-  let temp = '<div class="pixgallery-child-box" style="flex-basis:{dim}" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-box" style="height:{dim};" src="{url}"></a></div>';
-
-	let newValues = '', limitItem = urls.length;
-	for (let i = 0; i < limitItem; ++i) {
-
-    if(caption === null) {
-     newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace("{id}",el.id).replace("title=\"{caption}\"","");
-    } else {
-      if(caption[i] === null) {
-        newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace("{id}",el.id).replace("title=\"{caption}\"","");
-      } else {
-        newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace("{id}",el.id).replace("{caption}",caption[i]);
-      }
-    }
-	}
-
-	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery-box" style="gap:' + gap + ';">' + newValues + '</div>';
-  var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
+  if(x.layout === "grid") pixgallery_grid(el,x);
+  if(x.layout === "gridfixed") pixgallery_gridfixed(el,x);
+  if(x.layout === "masonry") pixgallery_masonry(el,x);
+  if(x.layout === "justified") pixgallery_justified(el,x);
+  if(x.layout === "elastic") pixgallery_elastic(el,x);
+  if(x.layout === "rhombus") pixgallery_rhombus(el,x);
 }
 
 /* flex grid parent and children divs have images as img -------------------- */
@@ -75,12 +51,81 @@ function pixgallery_grid(el,x){
   let caption = x.caption;
   let dim = x.dim;
   let gap = x.gap;
+  let borderRadius = x.border_radius;
 
-  let temp = '<div class="pixgallery-child-grid" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-grid" style="height:{dim};" src="{url}"></a></div>';
+  let temp = '<div class="pixgallery-child pixgallery-child-grid" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-grid" style="border-radius:{borderRadius};height:{dim};" src="{url}"></a></div>';
 
 	let newValues = '', limitItem = urls.length;
 	for (let i = 0; i < limitItem; ++i) {
+    if(caption == null || caption[i] == null) {
+      newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("title=\"{caption}\"","");
+    } else {
+      newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("{caption}",caption[i]);
+    }
+	}
 
+	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery pixgallery-gallery-grid" style="gap:' + gap + ';grid-template-columns: repeat(auto-fit, minmax(' + dim + ',1fr));grid-auto-rows:' + dim + ';">' + newValues + '</div>';
+  var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
+}
+
+/* flex grid parent and children divs have images as img -------------------- */
+function pixgallery_gridfixed(el,x){
+
+  let urls = x.path;
+  let caption = x.caption;
+  let dim = x.dim;
+  let gap = x.gap;
+  let borderRadius = x.border_radius;
+
+  let temp = '<div class="pixgallery-child pixgallery-child-gridfixed" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-gridfixed" style="border-radius:{borderRadius};height:{dim};" src="{url}"></a></div>';
+
+	let newValues = '', limitItem = urls.length;
+	for (let i = 0; i < limitItem; ++i) {
+    if(caption == null || caption[i] == null) {
+      newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("title=\"{caption}\"","");
+    } else {
+      newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("{caption}",caption[i]);
+    }
+	}
+
+	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery pixgallery-gallery-gridfixed" style="gap:' + gap + ';gridfixed-template-columns: repeat(auto-fit, ' + dim + ');gridfixed-auto-rows:' + dim + ';">' + newValues + '</div>';
+  var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
+}
+
+/* flex grid parent and children divs have images as img -------------------- */
+function pixgallery_rhombus(el,x){
+
+  let urls = x.path;
+  let caption = x.caption;
+  let gap = x.gap;
+
+  let temp = '<div class="pixgallery-child pixgallery-child-rhombus" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-rhombus" src="{url}"></a></div>';
+
+	let newValues = '', limitItem = urls.length;
+	for (let i = 0; i < limitItem; ++i) {
+    if(caption == null || caption[i] == null) {
+      newValues += temp.replace(/\{url\}/g, urls[i]).replace("{id}",el.id).replace("title=\"{caption}\"","");
+    } else {
+      newValues += temp.replace(/\{url\}/g, urls[i]).replace("{id}",el.id).replace("{caption}",caption[i]);
+    }
+	}
+
+  document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery pixgallery-gallery-rhombus" style="gap:' + gap + ';">' + newValues + '</div>';
+  var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
+}
+
+/* flex grid parent and children divs have images as img -------------------- */
+function pixgallery_elastic(el,x){
+
+  let urls = x.path;
+  let caption = x.caption;
+  let dim = x.dim;
+  let gap = x.gap;
+
+  let temp = '<div class="pixgallery-child pixgallery-child-elastic" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-elastic" src="{url}"></a></div>';
+
+	let newValues = '', limitItem = urls.length;
+	for (let i = 0; i < limitItem; ++i) {
     if(caption == null || caption[i] == null) {
       newValues += temp.replace(/\{dim\}/g, dim).replace(/\{url\}/g, urls[i]).replace("{id}",el.id).replace("title=\"{caption}\"","");
     } else {
@@ -88,6 +133,54 @@ function pixgallery_grid(el,x){
     }
 	}
 
-	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery-grid" style="gap:' + gap + ';grid-template-columns: repeat(auto-fit, minmax(' + dim + ',1fr));grid-auto-rows:' + dim + ';">' + newValues + '</div>';
+	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery pixgallery-gallery-elastic" style="height:' + dim + ';gap:' + gap + ';">' + newValues + '</div>';
+  var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
+}
+
+/* flex grid parent and children divs have images as img -------------------- */
+function pixgallery_masonry(el,x){
+
+  let urls = x.path;
+  let caption = x.caption;
+  let dim = x.dim;
+  let gap = x.gap;
+  let borderRadius = x.border_radius;
+
+  let temp = '<div class="pixgallery-child pixgallery-child-masonry" id="pixgallery-{id}" style="margin-bottom:{gap};"><a href="{url}" title="{caption}"><img class="pixgallery-image-masonry" style="border-radius:{borderRadius};" src="{url}"></a></div>';
+
+	let newValues = '', limitItem = urls.length;
+	for (let i = 0; i < limitItem; ++i) {
+    if(caption == null || caption[i] == null) {
+      newValues += temp.replace(/\{gap\}/g, gap).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("title=\"{caption}\"","");
+    } else {
+      newValues += temp.replace(/\{gap\}/g, gap).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("{caption}",caption[i]);
+    }
+	}
+
+	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery pixgallery-gallery-masonry" style="column-gap:' + gap + ';column-width:' + dim + ';">' + newValues + '</div>';
+  var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
+}
+
+/* flex grid parent and children divs have images as img -------------------- */
+function pixgallery_justified(el,x){
+
+  let urls = x.path;
+  let caption = x.caption;
+  let dim = x.dim;
+  let gap = x.gap;
+  let borderRadius = x.border_radius;
+
+  let temp = '<div class="pixgallery-child pixgallery-child-justified" id="pixgallery-{id}"><a href="{url}" title="{caption}"><img class="pixgallery-image-justified" style="height:{dim};border-radius:{borderRadius};" src="{url}"></a></div>';
+
+	let newValues = '', limitItem = urls.length;
+	for (let i = 0; i < limitItem; ++i) {
+    if(caption == null || caption[i] == null) {
+      newValues += temp.replace(/\{dim\}/g, dim).replace(/\{gap\}/g, gap).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("title=\"{caption}\"","");
+    } else {
+      newValues += temp.replace(/\{dim\}/g, dim).replace(/\{gap\}/g, gap).replace(/\{url\}/g, urls[i]).replace(/\{borderRadius\}/g, borderRadius).replace("{id}",el.id).replace("{caption}",caption[i]);
+    }
+	}
+
+	document.getElementById(el.id).innerHTML = '<div class="pixgallery-gallery pixgallery-gallery-justified" style="column-gap:' + gap + ';">' + newValues + '</div>';
   var lightbox = new SimpleLightbox({elements: '#pixgallery-' + el.id + ' a'});
 }
