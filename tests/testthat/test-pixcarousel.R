29bc2751e1f5629e18acc2f3697dcbdfd1a04e7f
Roy Francis <roymathewfrancis@gmail.com>
2025-11-01 17:28:57 +0100

    Added tests

---
+++ tests/testthat/test-pixcarousel.R
@@ -0,0 +1,129 @@
+library(testthat)
+library(pixture)
+
+# setup ------------------------------------------------------------------------
+
+paths <- c(
+  "https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/7604425/pexels-photo-7604425.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/4666748/pexels-photo-4666748.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/4932184/pexels-photo-4932184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/4210900/pexels-photo-4210900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/3126574/pexels-photo-3126574.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/1376201/pexels-photo-1376201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/7919/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/2437291/pexels-photo-2437291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/1679772/pexels-photo-1679772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/1183099/pexels-photo-1183099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/1813513/pexels-photo-1813513.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/931018/pexels-photo-931018.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/267074/pexels-photo-267074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/4622893/pexels-photo-4622893.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+)
+
+captions <- c("night", "forest", "shark", "coconut trees", "flowers", "field", "misty", "leaves", "sunset", "mountains", "swamp", "rainstorm", "leaves", "beach", "leopard", "sunflower")
+
+# pixcarousel -------------------------------------------------------------------
+
+test_that("No input", {
+  expect_error(pixcarousel())
+})
+
+test_that("Default",{
+  expect_no_error(pixcarousel(paths))
+  obj <- pixcarousel(paths)
+  expect_equal(class(obj), c("pixcarousel", "htmlwidget"))
+  expect_equal(unlist(obj$x$path), paths)
+  expect_equal(length(obj$x$path), length(paths))
+})
+
+test_that("Captions",{
+  expect_no_error(pixcarousel(paths, caption = captions))
+  obj <- pixcarousel(paths, caption = captions)
+  expect_equal(length(obj$x$caption), length(paths))
+  expect_error(pixcarousel(paths, caption = captions[-1]))
+})
+
+test_that("Captions with NA", {
+  captions1 <- captions
+  captions1[2] <- NA
+  expect_no_error(pixcarousel(paths, caption = captions1))
+  expect_error(pixcarousel(paths, caption = NA))
+  expect_no_error(pixcarousel(paths, caption = NULL))
+})
+
+test_that("Captions with all NA", {
+  captions1 <- rep(NA,length(paths))
+  expect_no_error(pixcarousel(paths, caption = captions1))
+})
+
+test_that("Captions as path names", {
+  paths1 <- paths
+  names(paths1) <- captions
+  expect_no_error(pixcarousel(paths1))
+})
+
+test_that("Captions and captions as path names", {
+  paths1 <- paths
+  names(paths1) <- captions
+  expect_no_error(pixcarousel(paths1, caption = captions))
+})
+
+test_that("Captions caption_valign", {
+  expect_no_error(pixcarousel(paths, caption = captions, caption_valign = "top"))
+  expect_no_error(pixcarousel(paths, caption = captions, caption_valign = "center"))
+  expect_no_error(pixcarousel(paths, caption = captions, caption_valign = "bottom"))
+  expect_error(pixcarousel(paths, caption = captions, caption_valign = "bla"))
+  expect_error(pixcarousel(paths, caption = captions, caption_valign = NA))
+  expect_error(pixcarousel(paths, caption = captions, caption_valign = NULL))
+})
+
+test_that("Captions caption_halign", {
+  expect_no_error(pixcarousel(paths, caption = captions, caption_halign = "left"))
+  expect_no_error(pixcarousel(paths, caption = captions, caption_halign = "center"))
+  expect_no_error(pixcarousel(paths, caption = captions, caption_halign = "right"))
+  expect_error(pixcarousel(paths, caption = captions, caption_halign = "bla"))
+  expect_error(pixcarousel(paths, caption = captions, caption_halign = NA))
+  expect_error(pixcarousel(paths, caption = captions, caption_halign = NULL))
+})
+
+test_that("Captions and link", {
+  expect_no_error(pixcarousel(paths, caption = captions, link = FALSE))
+  expect_no_error(pixcarousel(paths, caption = captions, link = rep("bla",length(captions))))
+  expect_error(pixcarousel(paths, caption = captions, link = "bla"))
+  expect_error(pixcarousel(paths, caption = captions, link = NA))
+  expect_error(pixcarousel(paths, caption = captions, link = NULL))
+  expect_error(pixcarousel(paths, caption = captions, link = c(T,F)))
+})
+
+test_that("Slides", {
+  expect_no_error(pixcarousel(paths, slides_to_show = 5, slides_to_scroll = 2))
+})
+
+test_that("Other", {
+  expect_no_error(pixcarousel(paths, draggable = TRUE, show_buttons = FALSE, show_dots = FALSE, scroll_lock = FALSE, rewind = TRUE))
+})
+
+test_that("Dimensions", {
+  expect_no_error(pixcarousel(paths, h = "20px"))
+  expect_no_error(pixcarousel(paths, h = NA))
+  expect_no_error(pixcarousel(paths, h = NULL))
+})
+
+test_that("Shuffle default", {
+  expect_no_error(pixcarousel(paths,shuffle=TRUE))
+  obj <- pixcarousel(paths,shuffle=TRUE)
+  expect_equal(length(obj$x$path), length(paths))
+})
+
+test_that("Shuffle with caption", {
+  expect_no_error(pixcarousel(paths, caption = captions, shuffle = TRUE))
+  obj <- pixcarousel(paths, caption = captions, shuffle = TRUE)
+  expect_equal(length(obj$x$path), length(paths))
+})
+
+test_that("Shuffle incorrect input", {
+  expect_error(pixcarousel(paths, shuffle = NA))
+  expect_error(pixcarousel(paths, shuffle = NULL))
+})
